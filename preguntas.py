45056db33674d1e42244add695e3602cc2ea68a6
Danilo Osorio <damosorioco@unal.edu.co>
2022-05-26 23:41:40 -0500

    final

--- preguntas.py
+++ preguntas.py
@@ -12,6 +12,7 @@ selección de las n variables más relevantes usando una prueba f.
 import pandas as pd
 import numpy as np
 from sklearn.feature_selection import f_regression
+from sklearn.model_selection import GridSearchCV
 
 
 def pregunta_01():
@@ -147,7 +148,7 @@ def pregunta_04():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Obtenga el pipeline optimo de la pregunta 3.
     gridSearchCV = pregunta_03()
@@ -156,20 +157,20 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = ____.____(____)
-    y_test_pred = ____.____(____)
+    y_train_pred = GridSearchCV.predict(X_train)
+    y_test_pred = gridSearchCV.predict(X_test)
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    mse_train = ____(
-        _____,
-        _____,
+    mse_train = mean_squared_error(
+        y_train,
+        y_train_pred,
     ).round(2)
 
-    mse_test = ____(
-        _____,
-        _____,
+    mse_test = mean_squared_error(
+        y_test,
+        y_test_pred,
     ).round(2)
 
     # Retorne el error cuadrático medio para entrenamiento y prueba
